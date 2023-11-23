436623cbd03542b14f8b8e6e565fc2d3c5a14328
Anjan Prasad <anjanprasad112@gmail.com>
2023-11-23 14:41:19 +0530

    vercel file updated

--- music-server/server.js
+++ music-server/server.js
@@ -11,7 +11,7 @@ app.use('/music',router);
 mongoose.connect("mongodb+srv://anjanprasad112:a0zDJ7ESYOmxDXmM@cluster0.k0ddy8s.mongodb.net/?retryWrites=true&w=majority").then(() => {
     console.log("Connected to database!");
 }).then(() => {
-// app.listen(5000);
+app.listen(5000);
 console.log("hosted successfully");
 })
 .catch((err) => {
