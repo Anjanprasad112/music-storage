e546e1797d73ab7944be04caf5029d5cf46f3dc3
Anjan Prasad <anjanprasad112@gmail.com>
2023-11-23 15:23:07 +0530

    api updated

--- music-server/server.js
+++ music-server/server.js
@@ -12,8 +12,8 @@ app.use(cors(
     }
     ));
     
-    app.use('/',router);
     app.use(express.json());
+app.use('/api',router);
 mongoose.connect("mongodb+srv://anjanprasad112:a0zDJ7ESYOmxDXmM@cluster0.k0ddy8s.mongodb.net/?retryWrites=true&w=majority").then(() => {
     console.log("Connected to database!");
 }).then(() => {
