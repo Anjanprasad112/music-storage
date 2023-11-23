b82140934458b2e70b11f38d8a133c28426d7064
Anjan Prasad <anjanprasad112@gmail.com>
2023-11-23 15:23:35 +0530

    cors updated

--- music-server/server.js
+++ music-server/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors(
     {
-        origin: [""],
+        origin: ["*"],
         methods: ["POST","GET"],
         credentials: true
     }
