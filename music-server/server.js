768d509a79ede16c679d7799c728361de3c3640c
Anjan Prasad <anjanprasad112@gmail.com>
2023-11-23 15:09:01 +0530

    eno ondh

--- music-server/server.js
+++ music-server/server.js
@@ -4,16 +4,16 @@ const router = require('./routes/music-routes');
 
 const cors = require('cors');
 const app = express();
-app.use(express.json());
 app.use(cors(
     {
         origin: [""],
         methods: ["POST","GET"],
         credentials: true
     }
-));
-
-app.use('https://music-storage-five.vercel.app/',router);
+    ));
+    
+    app.use('/',router);
+    app.use(express.json());
 mongoose.connect("mongodb+srv://anjanprasad112:a0zDJ7ESYOmxDXmM@cluster0.k0ddy8s.mongodb.net/?retryWrites=true&w=majority").then(() => {
     console.log("Connected to database!");
 }).then(() => {
